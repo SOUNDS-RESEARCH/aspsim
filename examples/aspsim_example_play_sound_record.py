046d2ebd1b9059e22c27a315a54a09060a4e2bc2
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-04-21 18:00:04 +0200

    added examples

---
+++ examples/aspsim_example_play_sound_record.py
@@ -0,0 +1,29 @@
+import numpy as np
+from pathlib import Path
+
+from aspsim.simulator import SimulatorSetup
+from aspsim.processor import AudioProcessor
+import aspsim.signal.sources as src
+import aspsim.diagnostics.diagnostics as dg
+
+class PlaySoundProcessor(AudioProcessor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.src = src.WhiteNoiseSource(1,1)
+        self.diag.add_diagnostic("loudspeaker power", dg.SignalPower("ls", self.sim_info, self.block_size))
+    
+    def process(self, num_samples):
+        self.sig["ls"][:, self.idx:self.idx+num_samples] = self.src.get_samples(num_samples)
+
+bs = 64
+
+setup = SimulatorSetup(Path(__file__).parent.joinpath("figs"))
+setup.add_controllable_source("ls", np.array([[1,0,0]]))
+setup.add_mics("mic", np.array([[0,0,0]]))
+
+sim = setup.create_simulator()
+proc = PlaySoundProcessor(sim.sim_info, sim.arrays, bs)
+proc.diag.add_diagnostic("loudspeaker signal", dg.RecordSignal("ls", sim.sim_info, bs))
+
+sim.add_processor(proc)
+sim.run_simulation()
