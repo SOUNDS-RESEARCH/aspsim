cec778e4bb57ffbf2b8204a655f35d8368a8811c
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-04-21 08:37:01 +0200

    name changes

--- aspsim/room/roomimpulseresponse.py
+++ aspsim/room/roomimpulseresponse.py
@@ -37,59 +37,59 @@ def ir_point_source_3d(pos_from, pos_to, samplerate, c, max_order=25):
     return ir
 
 
-def ir_point_source_2d(fromPos, toPos, samplerate, c, extraLen=100, maxOrder=25):
-    dist = distfuncs.cdist(fromPos, toPos)
-    sampleDelay = (samplerate * dist) / c
+def ir_point_source_2d(pos_from, pos_to, samplerate, c, extra_len=100, max_order=25):
+    dist = distfuncs.cdist(pos_from, pos_to)
+    sample_delay = (samplerate * dist) / c
 
-    delayFiltLen = int(np.max(sampleDelay)) + 1 + (maxOrder // 2) + 1
-    maxFiltLen = delayFiltLen + extraLen - 1
-    ir = np.zeros((dist.shape[0], dist.shape[1], maxFiltLen))
+    delay_filt_len = int(np.max(sample_delay)) + 1 + (max_order // 2) + 1
+    max_filt_len = delay_filt_len + extra_len - 1
+    ir = np.zeros((dist.shape[0], dist.shape[1], max_filt_len))
     for row in range(dist.shape[0]):
         for col in range(dist.shape[1]):
-            irFromImpact = 1 / (
+            ir_from_impact = 1 / (
                 2
                 * np.pi
                 * np.sqrt(
                     c ** 2
-                    * ((sampleDelay[row, col] + 0.5 + np.arange(extraLen)) / samplerate)
+                    * ((sample_delay[row, col] + 0.5 + np.arange(extra_len)) / samplerate)
                     ** 2
                     - (dist[row, col] ** 2)
                 )
             )
-            correctDelay = _frac_delay_lagrange_filter(
-                sampleDelay[row, col], maxOrder, delayFiltLen
+            correct_delay = _frac_delay_lagrange_filter(
+                sample_delay[row, col], max_order, delay_filt_len
             )
-            fullIr = np.convolve(irFromImpact, correctDelay, "full")
+            ir_full = np.convolve(ir_from_impact, correct_delay, "full")
 
-            ir[row, col, :] = fullIr
+            ir[row, col, :] = ir_full
     return ir
 
 
 
-def _frac_delay_lagrange_filter(delay, maxOrder, maxFiltLen):
+def _frac_delay_lagrange_filter(delay, max_order, max_filt_len):
     """Generates fractional delay filter using lagrange interpolation
     if maxOrder would require a longer filter than maxFiltLen,
     maxFiltLen will will override and set a lower interpolation order"""
-    ir = np.zeros(maxFiltLen)
-    fracdly, dly = np.modf(delay)
-    order = int(np.min((maxOrder, 2 * dly, 2 * (maxFiltLen - 1 - dly))))
+    ir = np.zeros(max_filt_len)
+    frac_dly, dly = np.modf(delay)
+    order = int(np.min((max_order, 2 * dly, 2 * (max_filt_len - 1 - dly))))
 
-    delta = np.floor(order / 2) + fracdly
+    delta = np.floor(order / 2) + frac_dly
     h = _lagrange_interpol(order, delta)
 
     diff = delay - delta
-    startIdx = int(np.floor(diff))
-    ir[startIdx : startIdx + order + 1] = h
+    start_idx = int(np.floor(diff))
+    ir[start_idx : start_idx + order + 1] = h
     return ir
 
 
 def _lagrange_interpol(N, delta):
-    irLen = int(N + 1)
-    h = np.zeros(irLen)
+    ir_len = int(N + 1)
+    h = np.zeros(ir_len)
 
-    for n in range(irLen):
-        k = np.arange(irLen)
-        k = k[np.arange(irLen) != n]
+    for n in range(ir_len):
+        k = np.arange(ir_len)
+        k = k[np.arange(ir_len) != n]
 
         h[n] = np.prod((delta - k) / (n - k))
     return h
@@ -111,20 +111,20 @@ def ir_room_image_source_3d(
     verbose=False,
 ):
 
-    numFrom = pos_from.shape[0]
-    numTo = pos_to.shape[0]
-    ir = np.zeros((numFrom, numTo, ir_len))
+    num_from = pos_from.shape[0]
+    num_to = pos_to.shape[0]
+    ir = np.zeros((num_from, num_to, ir_len))
     room_center = np.array(room_center)
     room_size = np.array(room_size)
 
-    posOffset = room_size / 2 - room_center
+    pos_offset = room_size / 2 - room_center
 
     if rt60 > 0:
-        eAbsorption, maxOrder = pra.inverse_sabine(rt60, room_size)
-        maxOrder += 3
+        e_absorbtion, max_order = pra.inverse_sabine(rt60, room_size)
+        max_order += 3
     else:
-        eAbsorption = 0.9
-        maxOrder = 0
+        e_absorbtion = 0.9
+        max_order = 0
     # print("Energy Absorption: ", eAbsorption)
     # print("Max Order: ", maxOrder)
 
@@ -138,27 +138,27 @@ def ir_room_image_source_3d(
         if frac_dly_len < 20:
             print("WARNING: fractional delay length: ",frac_dly_len)
 
-    maxTruncError = np.NINF
-    maxTruncValue = np.NINF
-    maxNumIRAtOnce = 500
-    numComputed = 0
-    while numComputed < numTo:
+    max_trunc_error = np.NINF
+    max_trunc_value = np.NINF
+    max_num_ir_at_once = 500
+    num_computed = 0
+    while num_computed < num_to:
         room = pra.ShoeBox(
             room_size,
-            materials=pra.Material(eAbsorption),
+            materials=pra.Material(e_absorbtion),
             fs=samplerate,
-            max_order=maxOrder,
+            max_order=max_order,
             use_rand_ism = True, 
             max_rand_disp = 0.05
         )
         # room = pra.ShoeBox(roomSim, materials=pra.Material(e_absorption), fs=sampleRate, max_order=max_order)
 
-        for srcIdx in range(numFrom):
-            room.add_source((pos_from[srcIdx, :] + posOffset).T)
+        for src_idx in range(num_from):
+            room.add_source((pos_from[src_idx, :] + pos_offset).T)
 
-        block_size = np.min((maxNumIRAtOnce, numTo - numComputed))
+        block_size = np.min((max_num_ir_at_once, num_to - num_computed))
         mics = pra.MicrophoneArray(
-            (pos_to[numComputed : numComputed + block_size, :] + posOffset[None, :]).T,
+            (pos_to[num_computed : num_computed + block_size, :] + pos_offset[None, :]).T,
             room.fs,
         )
         room.add_microphone_array(mics)
@@ -166,31 +166,29 @@ def ir_room_image_source_3d(
         if verbose:
             print(
                 "Computing RIR {} - {} of {}".format(
-                    numComputed * numFrom + 1,
-                    (numComputed + block_size) * numFrom,
-                    numTo * numFrom,
+                    num_computed * num_from + 1,
+                    (num_computed + block_size) * num_from,
+                    num_to * num_from,
                 )
             )
         room.compute_rir()
-        for toIdx, receiver in enumerate(room.rir):
-            for fromIdx, singleRIR in enumerate(receiver):
-                irLenToUse = np.min((len(singleRIR), ir_len)) - min_dly
-                #print(irLenToUse)
-                ir[fromIdx, numComputed + toIdx, :irLenToUse] = np.array(singleRIR)[
-                    min_dly:irLenToUse+min_dly
+        for to_idx, receiver in enumerate(room.rir):
+            for from_idx, single_rir in enumerate(receiver):
+                ir_len_to_use = np.min((len(single_rir), ir_len)) - min_dly
+                ir[from_idx, num_computed + to_idx, :ir_len_to_use] = np.array(single_rir)[
+                    min_dly:ir_len_to_use+min_dly
                 ]
-        numComputed += block_size
+        num_computed += block_size
 
-        # print("RT 60: ", room.measure_rt60())
         if calculate_metadata:
             truncError, truncValue = calc_truncation_info(room.rir, ir_len)
-            maxTruncError = np.max((maxTruncError, truncError))
-            maxTruncValue = np.max((maxTruncValue, truncValue))
+            max_trunc_error = np.max((max_trunc_error, truncError))
+            max_trunc_value = np.max((max_trunc_value, truncValue))
 
     if calculate_metadata:
         metadata = {}
-        metadata["Max Normalized Truncation Error (dB)"] = maxTruncError
-        metadata["Max Normalized Truncated Value (dB)"] = maxTruncValue
+        metadata["Max Normalized Truncation Error (dB)"] = max_trunc_error
+        metadata["Max Normalized Truncated Value (dB)"] = max_trunc_value
         if rt60 > 0:
             try:
                 metadata["Measured RT60 (min)"] = np.min(room.measure_rt60())
@@ -201,43 +199,43 @@ def ir_room_image_source_3d(
         else:
             metadata["Measured RT60 (min)"] = 0
             metadata["Measured RT60 (max)"] = 0
-        metadata["Max ISM order"] = maxOrder
-        metadata["Energy Absorption"] = eAbsorption
+        metadata["Max ISM order"] = max_order
+        metadata["Energy Absorption"] = e_absorbtion
         return ir, metadata
     return ir
 
     
-def calc_truncation_info(allRIR, truncLen):
-    maxTruncError = np.NINF
-    maxTruncValue = np.NINF
-    for toIdx, receiver in enumerate(allRIR):
-        for fromIdx, singleRIR in enumerate(receiver):
-            irLen = np.min((len(singleRIR), truncLen))
-            truncError = calc_truncation_error(singleRIR, irLen)
-            truncValue = calc_truncation_value(singleRIR, irLen)
-            maxTruncError = np.max((maxTruncError, truncError))
-            maxTruncValue = np.max((maxTruncValue, truncValue))
-    return maxTruncError, maxTruncValue
+def calc_truncation_info(all_rir, trunc_len):
+    max_trunc_error = np.NINF
+    max_trunc_value = np.NINF
+    for to_idx, receiver in enumerate(all_rir):
+        for from_idx, single_rir in enumerate(receiver):
+            ir_len = np.min((len(single_rir), trunc_len))
+            trunc_error = calc_truncation_error(single_rir, ir_len)
+            trunc_value = calc_truncation_value(single_rir, ir_len)
+            max_trunc_error = np.max((max_trunc_error, trunc_error))
+            max_trunc_value = np.max((max_trunc_value, trunc_value))
+    return max_trunc_error, max_trunc_value
 
 
-def calc_truncation_error(RIR, truncLen):
-    totPower = np.sum(RIR ** 2)
-    truncPower = np.sum(RIR[truncLen:] ** 2)
-    truncError = truncPower / totPower
-    return 10 * np.log10(truncError)
+def calc_truncation_error(rir, trunc_len):
+    tot_power = np.sum(rir ** 2)
+    trunc_power = np.sum(rir[trunc_len:] ** 2)
+    trunc_error = trunc_power / tot_power
+    return 10 * np.log10(trunc_error)
 
 
-def calc_truncation_value(RIR, truncLen):
-    if len(RIR) <= truncLen:
+def calc_truncation_value(rir, trunc_len):
+    if len(rir) <= trunc_len:
         return -np.inf
-    maxTruncValue = np.max(np.abs(RIR[truncLen:]))
-    maxValue = np.max(np.abs(RIR))
-    return 20 * np.log10(maxTruncValue / maxValue)
+    max_trunc_value = np.max(np.abs(rir[trunc_len:]))
+    max_value = np.max(np.abs(rir))
+    return 20 * np.log10(max_trunc_value / max_value)
 
 
-def show_rt60(multiChannelIR):
-    for i in range(multiChannelIR.shape[0]):
-        for j in range(multiChannelIR.shape[1]):
-            singleIR = multiChannelIR[i, j, :]
-            rt = pra.experimental.rt60.measure_rt60(singleIR)
+def show_rt60(multi_channel_ir):
+    for i in range(multi_channel_ir.shape[0]):
+        for j in range(multi_channel_ir.shape[1]):
+            single_ir = multi_channel_ir[i, j, :]
+            rt = pra.experimental.rt60.measure_rt60(single_ir)
             print("rt60 is: ", rt)
