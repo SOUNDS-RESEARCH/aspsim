b4dabc2b2f69a6dd3a65daeb584659c4b0caa3aa
Jesper Brunnstroem <jbrunnst@esat.kuleuven.ac.be>
2023-12-11 11:21:00 +0100

    added reverb option to example on directional mics

--- examples/example_directional_mic.py
+++ examples/example_directional_mic.py
@@ -6,8 +6,9 @@ import copy
 from aspsim.simulator import SimulatorSetup
 import aspsim.signal.sources as src
 import aspsim.diagnostics.diagnostics as dg
-import aspsim.room.trajectory as traj
 import aspsim.room.generatepoints as gp
+
+REVERB = True
 
 # Choose where figures should be saved and create a SimulatorSetup object
 fig_path = Path(__file__).parent.joinpath("figs")
@@ -15,20 +16,22 @@ fig_path.mkdir(exist_ok=True)
 setup = SimulatorSetup(fig_path)
 
 # Adjust config values
-setup.sim_info.tot_samples = 2000
+setup.sim_info.tot_samples = 5000
 setup.sim_info.samplerate = 1000
-setup.sim_info.rt60 = 0
 setup.sim_info.max_room_ir_length = 1000
 setup.sim_info.array_update_freq = 1
 setup.sim_info.save_source_contributions = True
-
-setup.sim_info.export_frequency = 2000
+setup.sim_info.export_frequency = setup.sim_info.tot_samples
 setup.sim_info.plot_output = "pdf"
+if REVERB:
+    setup.sim_info.rt60 = 0.25
+else:
+    setup.sim_info.rt60 = 0.0
 
 #Setup sources and microphones
-num_src = 25
+num_src = 50
 source_angles = np.linspace(0, 2*np.pi, num_src)
-(x, y) = gp.pol2cart(np.ones(num_src), source_angles)
+(x, y) = gp.pol2cart(0.5*np.ones(num_src), source_angles)
 pos_src = np.zeros((num_src, 3))
 pos_src[:,0] = x
 pos_src[:,1] = y
@@ -47,6 +50,7 @@ sim.diag.add_diagnostic("microphone_signal", dg.RecordSignal("mic", sim.sim_info
 
 sim.run_simulation()
 
+# Calculate average power from each loudspeaker recorded by the microphone
 recorded_power = []
 for i in range(num_src):
     rec_sig = np.load(sim.folder_path.joinpath(f"recorded_ls_sig_{i}_{sim.sim_info.tot_samples}.npz"))
@@ -54,6 +58,7 @@ for i in range(num_src):
     recorded_power.append(np.mean(rec_sig**2))
 recorded_power = recorded_power / np.max(recorded_power)
 
+# Calculate what the true power distribution should be from a cardioid microphone
 num_true_cardioid_angles = 300
 true_cardioid_angles = np.linspace(0, 2*np.pi, num_true_cardioid_angles)
 (x, y) = gp.pol2cart(np.ones(num_true_cardioid_angles), true_cardioid_angles)
@@ -62,6 +67,7 @@ pos_true_cardioid[:,0] = x
 pos_true_cardioid[:,1] = y
 true_cardoid_power = (0.5 + 0.5 * np.sum(directivity_dir * pos_true_cardioid, axis=-1))**2
 
+# Plot the results
 fig, ax = plt.subplots(1,1, figsize=(8,6), subplot_kw={'projection': 'polar'})
 ax.plot(true_cardioid_angles, true_cardoid_power, label="True cardioid power", alpha=0.4, linewidth=5)
 ax.plot(source_angles, recorded_power, "-o", label="Recorded power", alpha=0.9)
